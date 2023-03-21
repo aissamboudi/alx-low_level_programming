0c198bd43aeac2d7a582a5b9c1cfaadeea3ee627
aissamboudi <aissamboudi@gmail.com>
2023-03-21 15:45:42 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+/**
+ * add - number + number
+ * @a: first number input
+ * @b: second number input
+ * Return: a + b
+ */
+int add(int one, int two)
+{
+return (one + two);
+}
