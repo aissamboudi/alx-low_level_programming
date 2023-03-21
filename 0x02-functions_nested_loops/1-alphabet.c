b11e2754b2de7a2a7bc78b42236a4020d9220032
aissamboudi <aissamboudi@gmail.com>
2023-03-21 08:01:09 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,11 @@
 
 void print_alphabet(void)
 {
-	char *str = "abcdefghijklmnopqrstuvwxyz";
-	
-	while (*str)
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
+char *str = "abcdefghijklmnopqrstuvwxyz";	
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
 }
