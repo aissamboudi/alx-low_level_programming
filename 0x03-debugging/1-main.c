0033e5ebe42dd32b89bb36623a56483dc46ec163
aissamboudi <aissamboudi@gmail.com>
2023-03-23 19:57:37 -0700

    2-largest_number

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,10 @@
 
 int main(void)
 {
-int i = 0;
-
-printf("Infinite loop incoming :(\n");
-
-/*
-*while (i < 10)
-*{
-*putchar(i);
-*}
-*/
-printf("Infinite loop avoided! \\o/\n");
+	int i = 0;
+	
+	printf("Infinite loop incoming :(\n";
+	printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
