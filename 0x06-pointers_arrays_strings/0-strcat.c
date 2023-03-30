854cf9488fecbb987f1454f9eb1775316a213b54
aissamboudi <aissamboudi@gmail.com>
2023-03-30 06:29:29 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,20 @@
 #include "main.c"
 /**
- * _strcat - 
+ * _strcat - concat two
  * @dest: first string
  * @src: second string
  * Return: concatenated string
  */
 char *_strcat(char *dest, char *src)
 {
-	while(*dest)
+	while (*dest)
 	{
-                if(*dest == '\0')
-                {
-                        *dest = *src;
-                        src++;
-                }
-                dest++;
+		if (*dest == '\0')
+		{
+			*dest = *src;
+			src++;
+		}
+		dest++;
 	}
 	return (dest);
 }
