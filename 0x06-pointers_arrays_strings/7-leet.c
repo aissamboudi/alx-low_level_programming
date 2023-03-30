b2db228819e3cf48e4b5ff244ae85e4f13f84549
aissamboudi <aissamboudi@gmail.com>
2023-03-30 23:24:06 +0000

    Task 6

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - 1337's string
+ * @s: target string
+ * Return: pointer s
+ */
+char *leet(char *s)
+{
+	int cpt = 0;
+	char *alp = "aAeEoOtTlL";
+	char *num = "4433007711";
+
+	while (*(s + cpt))
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			if (*(s + cpt) == *(alp + i))
+				*(s + cpt) = *(num + i);
+		} 
+		
+	}
+
+	return (s);
+}
