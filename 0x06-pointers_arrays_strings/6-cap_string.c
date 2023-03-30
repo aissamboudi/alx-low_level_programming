dcfc8083e24a5d2b8f25405c905d7f0dbfcc5413
aissamboudi <aissamboudi@gmail.com>
2023-03-30 22:30:42 +0000

    Task 5

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,22 @@ char *cap_string(char *s)
 {
 	int cpt = 0;
 
+	if (*s >= 97 && *s <= 122)
+		*s = *s - 32;
 	while (*(s + cpt))
 	{
 		if (*(s + cpt) == ' ' || *(s + cpt) == '\t' || *(s + cpt) == '\n'
 				|| *(s + cpt) == ',' || *(s + cpt) == ';' || *(s + cpt) == '.'
 				|| *(s + cpt) == '!' || *(s + cpt) == '?' || *(s + cpt) == '"'
-				|| *(s + cpt) == '(' || *(s + cpt) == ')' || *(s + cpt) == '{'
-				|| *(s + cpt) == '}')
+				|| *(s + cpt) == '(' || *(s + cpt) == ')'
+				|| *(s + cpt) == '{' || *(s + cpt) == '}')
 		{
 			cpt++;
-			if (*(s + cpt) >= 97 && *(s + cpt) <= 122)i
+			if (*(s + cpt) >= 97 && *(s + cpt) <= 122)
 				*(s + cpt) = *(s + cpt) - 32;
-			else if (*(s + cpt) >= 65 && *(s + cpt) <= 90)
-				cpt++;
-			cpt--;
 		}
 		cpt++;
 	}
-	if (*s >= 97 && *s <= 122)
-		*s = *s - 32;
 
 	return (s);
 }
