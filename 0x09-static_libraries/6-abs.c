f526452c0f3753d9a2954d609eb6fda34c849852
aissamboudi <aissamboudi@gmail.com>
2023-04-10 22:19:32 +0000

    Task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Absolute value of an integer
+ * @d: integer input
+ * Return: absolute value d number
+ */
+int _abs(int d)
+{
+if (d > 0)
+return (d * 1);
+else if (d < 0)
+return (d * -1);
+else
+return (0);
+}
