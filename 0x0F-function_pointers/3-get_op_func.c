31a7a307e77c1690373ff2686eec2a453fff35b4
aissamboudi <aissamboudi@gmail.com>
2023-04-21 03:13:38 +0000

    Task 04

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include "3-op_functions.c"
+
+/**
+ * get_op_func - returns a pointer to the function
+ * that corresponds to the operator given as a parameter
+ * @s: pointer to char
+ * Return: always 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t opts[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int a;
+
+	if (s[0] != '\0')
+	{
+		for (a = 0; a < 5; a++)
+		{
+			if (*(opts[a].op) == *s)
+				return (opts[a].f);
+		}
+	}
+	return (0);
+}
