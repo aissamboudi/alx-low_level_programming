5dcf969b7a21212de3742557db43441794aaa2dd
aissamboudi <aissamboudi@gmail.com>
2023-04-21 02:59:38 +0000

    Task 02

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,20 +4,20 @@
 
 /**
  * print_numbers - prints numbers, followed by a new line.
- * @separator: Separator
- * @n: Count of given params
- * Return: void
+ * @separator: Separator of numbers
+ * @n: Count of numbers
+ * Return: Nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list param_list;
+	va_list p_list;
 	unsigned int i;
 
-	va_start(param_list, n);
+	va_start(p_list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(param_list, int));
+		printf("%d", va_arg(p_list, int));
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
