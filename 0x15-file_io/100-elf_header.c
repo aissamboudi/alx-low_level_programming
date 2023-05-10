061ff8e102ecff2982b017c3f9599a02f2a231cb
aissamboudi <aissamboudi@gmail.com>
2023-05-10 01:02:13 +0100

    Task 05

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - displays the information contained in the ELF
  * header at the start of an ELF file
  * Return: 0
+ *
  */
-int main(void)
+ int main(int ac, char *av[])
 {
+	return (1);
 	return (0);
 }
