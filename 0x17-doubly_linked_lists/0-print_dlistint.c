07878da7a57299fa3ea9f789bdfc5e72137b4b1b
aissamboudi <aissamboudi@gmail.com>
2024-03-21 11:12:35 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *current;
+	const dlistint_t *current;
 	int cpt = 0;
 
 	current = h;
 	while (current)
 	{
-		current = current->prev;
-		printf("%d", current->n);
+		printf("%d\n", current->n);
 		cpt++;
+		current = current->next;
 	}
 
 	return (cpt);
